2e498e0167d7360e4f7fe5ef80e252702cf8c66a
Bugnone <bugnone.michael@gmail.com>
2021-01-28 23:36:09 +0100

    room

--- src/Server.java
+++ src/Server.java
@@ -1,6 +1,10 @@
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -11,10 +15,16 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server extends Thread {
+    private HashMap<String, String> rooms = new HashMap<>();
 
     @Override
     public void run() {
@@ -47,6 +57,65 @@ public class Server extends Thread {
                 }
             });
 
+            httpServer.createContext("/hello", exchange -> {
+
+                Document doc = Jsoup.connect("https://en.wikipedia.org/").get();
+                Elements newsHeadlines = doc.select("#mp-itn b a");
+                for (Element headline : newsHeadlines) {
+                    System.out.println(headline.attr("title"));
+                }
+
+                System.out.println("hello");
+            });
+
+            httpServer.createContext("/createRoom", exchange -> {
+                int randomCode = new Random().nextInt(10000);
+                String randomCode4digit = String.format("%04d", randomCode);
+                this.rooms.put(randomCode4digit.toUpperCase(), "https://fr.wikipedia.org/wiki/Star_Wars");
+
+                System.out.println("RANDOM ROOM NUMBER CREATED " + randomCode4digit);
+
+                try {
+                    byte[] bs = randomCode4digit.getBytes();
+                    exchange.sendResponseHeaders(200, bs.length);
+                    OutputStream os = exchange.getResponseBody();
+                    os.write(bs);
+                    os.close();
+
+                } catch (IOException e) {
+                    System.out.println();
+                }
+            });
+
+            httpServer.createContext("/room/", exchange -> {
+                Path path = Paths.get(exchange.getRequestURI().getPath());
+                String roomNumber = path.getFileName().toString();
+
+                if(this.rooms.containsKey(roomNumber.toUpperCase())) {
+                    try {
+                        HttpClient client = HttpClient.newHttpClient();
+                        HttpRequest request = HttpRequest.newBuilder()
+                                .uri(URI.create(this.rooms.get(roomNumber.toUpperCase())))
+                                .build();
+
+                        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                        String responseString = response.body();
+
+                        byte[] bs = responseString.getBytes(StandardCharsets.UTF_8);
+                        exchange.sendResponseHeaders(200, bs.length);
+                        OutputStream os = exchange.getResponseBody();
+                        os.write(bs);
+                        os.close();
+
+                    } catch (IOException | InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else {
+                    showError(exchange, 404, "Bad code");
+                }
+            });
+
             httpServer.setExecutor(executor);
             httpServer.start();
 
@@ -55,5 +124,17 @@ public class Server extends Thread {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void showError(HttpExchange httpExchange, int respCode, String errDesc) throws IOException {
+        String message = "HTTP error " + respCode + ": " + errDesc;
+        byte[] messageBytes = message.getBytes(StandardCharsets.UTF_8);
+
+        httpExchange.getResponseHeaders().set("Content-Type", "text/plain; charset=utf-8");
+        httpExchange.sendResponseHeaders(respCode, messageBytes.length);
+
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(messageBytes);
+        os.close();
     }
 }
