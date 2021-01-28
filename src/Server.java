d48a8773b1e64b747cb835ca34ae5cae3c4257ca
Bugnone <bugnone.michael@gmail.com>
2021-01-28 21:44:01 +0100

    link

--- src/Server.java
+++ src/Server.java
@@ -3,7 +3,14 @@ import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -17,10 +24,26 @@ public class Server extends Thread {
 
             httpServer.createContext("/test", new ServerHandler());
 
-            httpServer.createContext("/hello", new HttpHandler(){
-                @Override
-                public void handle(final HttpExchange exchange) throws IOException {
-                    System.out.println("test");
+            httpServer.createContext("/wiki/", exchange -> {
+                System.out.println(exchange.getRequestURI().getPath());
+                HttpClient client = HttpClient.newHttpClient();
+
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create("https://fr.wikipedia.org" + exchange.getRequestURI().getPath()))
+                        .build();
+
+                try {
+                    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                    String responseString = response.body();
+
+                    byte[] bs = responseString.getBytes(StandardCharsets.UTF_8);
+                    exchange.sendResponseHeaders(200, bs.length);
+                    OutputStream os = exchange.getResponseBody();
+                    os.write(bs);
+                    os.close();
+
+                } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
                 }
             });
 
